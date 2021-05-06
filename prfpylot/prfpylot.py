e97978716b7ef77654e9ff28a34c6e5ad9f7341e
Lena Feld <lena@imk-asf-modllf.fritz.box>
2021-05-06 17:10:39 +0200

    initial structure

---
+++ prfpylot/prfpylot.py
@@ -0,0 +1,11 @@
+class PRFpylot():
+    """docstring for PRFpylot"""
+    def __init__(self, arg):
+        super(PRFpylot, self).__init__()
+        self.arg = arg
+
+        # read input file
+        pass
+
+    def generate_template_file():
+        pass
