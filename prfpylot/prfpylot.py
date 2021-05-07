819256a71822cdcbf672e9f9b10806ff7e7bf9d8
Lena Feld <lena@imk-asf-modllf.fritz.box>
2021-05-07 16:10:27 +0200

    started implementing init

--- prfpylot/prfpylot.py
+++ prfpylot/prfpylot.py
@@ -1,11 +1,32 @@
+import yaml
+
+
 class PRFpylot():
     """docstring for PRFpylot"""
-    def __init__(self, arg):
-        super(PRFpylot, self).__init__()
-        self.arg = arg
-
+    def __init__(self, input_path="input.yml"):
         # read input file
-        pass
+        with open(input_path, "r") as f:
+            args = yaml.load(f)
+
+        # set parameters from input file
+        self.coordinates = args["coordinates"]
+
+        self.map_path = args["map_path"]
+        if self.map_path == "default":
+            # Insert the default map path!
+            pass
+        self.pt_path = args["pt_path"]
+        if self.pt_path == "default":
+            # Insert the default pt path!
+            pass
+
+        # set general parameters
+        self.tempfiles = {
+            "prep": None,
+            "pt": None,
+            "inv": None,
+            "pcxs": None
+        }
 
     def generate_template_file():
         pass
