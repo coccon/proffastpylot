f64b33236c4adc42691fef3da5f97e9308306398
Lena Feld <lena@imk-asf-modllf.fritz.box>
2021-05-17 15:46:57 +0200

    notes in run.py
    Former-commit-id: 6b8f94effb11608f6aec38984e3d40c7fda5a9bf

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+# TODO: optional input parameters: list of dates or start and end-date
+# python run.py --start xxx --stop xxx --input input.yml --sitename Sodankyla
+
 from prfpylot.prfpylot import Pylot
 
 MyPRFpylot = Pylot()
