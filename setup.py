6082b801e3ba22d253dfd35445924b70056be293
Lena Feld <lena.feld@kit.edu>
2021-10-19 14:51:35 +0300

    added setup.py and MIT Licence
    Former-commit-id: 4c787315827259193c4a7f9a7ea9a6c7764a98c0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="prfPylot",
+    packages=["prfpylot"],
+    author="Benedikt Herkommer, Lena Feld",
+    author_email="lena.feld@kit.edu, benedikt.herkommer@kit.edu",
+    description="Run PROFFAST with Python",
+    install_requires=[
+        "pandas",
+        ],
+    license='MIT',
+    )
