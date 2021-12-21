68f310076f8ae86d2c3e5a7afe9c9c2126fc153f
Lena Feld <lena.feld@kit.edu>
2021-12-21 13:08:41 +0100

    simplified example runscript
    Former-commit-id: 777d4ddb967fb2848c9ff3b635b5cb788b32fe2a

--- example/run.py
+++ example/run.py
@@ -1,21 +1,13 @@
-# TODO: optional input parameters: list of dates or start and end-date
-# python run.py --start xxx --stop xxx --input input.yml --sitename Sodankyla
-
+from prfpylot.create_inputfile import InputfileGenerator
 from prfpylot.pylot import Pylot
-import os
-import time
 
 if __name__ == "__main__":
-    # if __name__ == "__main__" is necessary for multiprocessing
-    try:
-        input_file = "input_sodankyla_example.yml"
-        MyPRFpylot = Pylot(input_file, logginglevel="info")
-        # MyPRFpylot.run(n_processes=2)
+
+    # create the input file for the example data
+    MyInputfileGenerator = InputfileGenerator()
+    MyInputfileGenerator.generate_sod_example()
         
-        #MyPRFpylot.run_preprocess(n_processes=2)
-        #MyPRFpylot.run_pcxs(n_processes=2)
-        MyPRFpylot.run_inv(n_processes=2)
-        MyPRFpylot.combine_results()
-    finally:
-        MyPRFpylot.clean_files()
-        
+    # run proffast using the prfPylot
+    input_file = "input_sodankyla_example.yml"
+    MyPylot = Pylot(input_file, logginglevel="debug")
+    MyPylot.run(n_processes=2)
