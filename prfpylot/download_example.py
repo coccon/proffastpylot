4927666a0e26633588ba0088c5818131cee635e3
Lena Feld <lena.feld@kit.edu>
2022-02-22 13:15:52 +0100

    added module docstrings containing License information to each module

--- prfpylot/download_example.py
+++ prfpylot/download_example.py
@@ -1,3 +1,25 @@
+"""Download_example is a module of PROFFASTpylot.
+
+Download the Sodankyla example data from the KIT website.
+
+License information:
+PROFFASTpylot - Running PROFFAST with Python
+Copyright (C)   2022    Lena Feld, Benedikt Herkommer, 
+                        Karlsruhe Institut of Technology (KIT)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import requests
 from tqdm import tqdm
 from zipfile import ZipFile
--- prfpylot/pressure.py
+++ prfpylot/pressure.py
@@ -1,5 +1,24 @@
-"""Module to hand the pressure to PROFFAST.
-The user can add own functions to handle the viarity of data formats.
+"""Pressure is a module of PROFFASTpylot.
+
+Hand the pressure to PROFFAST, add own functions to handle different
+data formats.
+
+License information:
+PROFFASTpylot - Running PROFFAST with Python
+Copyright (C)   2022    Lena Feld, Benedikt Herkommer, 
+                        Karlsruhe Institut of Technology (KIT)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import pandas as pd
