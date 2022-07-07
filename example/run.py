7a66f0d351a8e7fd201d3cec66e3ed969020807c
Benedikt Herkommer <benedikt.herkommer@kit.edu>
2022-07-07 08:40:26 +0200

    Bugfix run script for windows

--- example/run.py
+++ example/run.py
@@ -1,14 +1,23 @@
-# Check if example input data is already available on disk, if not download it.
-# This is not needed for your personal PROFASTpylot steering file
-from prfpylot.download_example import ExampleDownloadHandler
-ExampleDownloadHandler().check_and_download_example_data()
+"""
+This is the example run script for the Sodankyla example data using the
+PROFFASTpylot.
+To execute this file you have to change your working directory to be
+.../proffastpylot/example
+"""
 
-
-# The following part can be adapted to your own retrieval
-from prfpylot.pylot import Pylot
-
-input_file = "input_sodankyla_example.yml"
-MyPylot = Pylot(input_file, logginglevel="info")
-
+# It is important to use this if clause in windows to let the multiprocessing
+# work properly.
 if __name__ == "__main__":
+    # Check if example input data is already available on disk,
+    # if not download it.
+    # This is not needed for your personal PROFASTpylot run-file
+    from prfpylot.download_example import ExampleDownloadHandler
+    ExampleDownloadHandler().check_and_download_example_data()
+
+    # The following part can be adapted to your own retrieval
+    from prfpylot.pylot import Pylot
+
+    input_file = "input_sodankyla_example.yml"
+    MyPylot = Pylot(input_file, logginglevel="info")
+
     MyPylot.run(n_processes=2)
