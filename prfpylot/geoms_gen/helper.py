858384289405b0db42eb255d87c0453b1d12ac69
Benedikt Herkommer <benedikt.herkommer@kit.edu>
2023-04-12 07:22:00 +0200

    Add license information

--- prfpylot/geoms_gen/helper.py
+++ prfpylot/geoms_gen/helper.py
@@ -1,3 +1,28 @@
+"""GeomsGenHelper is a module of PROFFASTpylot.
+
+It contains several helper routines for the generation of
+hdf5 files.
+
+License information:
+PROFFASTpylot - Running PROFFAST with Python
+Copyright (C)   2022    Lena Feld, Benedikt Herkommer,
+                        Darko Dubravica,
+                        Karlsruhe Institut of Technology (KIT)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import yaml
 import sys
 import os
--- prfpylot/geoms_gen/writer.py
+++ prfpylot/geoms_gen/writer.py
@@ -1,3 +1,27 @@
+"""GeomsGenWriter is a module of PROFFASTpylot.
+
+Generate GEOMS HDF5 files.
+
+License information:
+PROFFASTpylot - Running PROFFAST with Python
+Copyright (C)   2022    Lena Feld, Benedikt Herkommer,
+                        Darko Dubravica,
+                        Karlsruhe Institut of Technology (KIT)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import h5py
 import os
 import re
