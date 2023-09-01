e213a9ff9418e1a6ddf7b336746483d2820441a0
Benedikt Herkommer <benedikt.herkommer@kit.edu>
2023-09-01 19:00:54 +0200

    Custom css file to imitate KIT Cooperate Identity

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+#!/home/ly1868/Temp_Pylot/docvenv/bin/python3
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -10,10 +11,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../prfpylot'))
 
+
+
 project = 'PROFFASTpylot'
-copyright = '2023, Lena Feld, Benedikt Herkommer'
+copyright = '2023, Lena Feld, Benedikt Herkommer Karlsruhe Institut of Technology'
 author = 'Lena Feld, Benedikt Herkommer'
 release = 'v1.2'
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -51,5 +55,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'haiku'
+
 html_static_path = ['_static']
+
+# With the following code the KIT logo can be included.
+# However, this is not working properly with the current custom CSS file!.
+# html_logo= '_static/images/kitlogo_200dpi.png'
+# html_theme_options = {
+#     'full_logo': False}
+
+html_css_files = ['css/custom.css']
 
