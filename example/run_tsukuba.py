3d59f0d880077f8ed04eaeb7234b2c8ba065c93f
Moritz Makowski <moritz.makowski@tum.de>
2026-03-19 14:49:52 +0100

    #1 Add Pytest cases (3)
    update heidelberg mobile, tsukuba, and hdf geoms examples

--- example/run_tsukuba.py
+++ example/run_tsukuba.py
@@ -1,5 +1,13 @@
-from prfpylot.pylot import Pylot
+import os
+import sys
 
-input_file = "proffastpylot_parameters_tsukuba.yml"
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from prfpylot.pylot import Pylot
+from prfpylot.constants import EXAMPLE_DIR
+
+
 if __name__ == "__main__":
+    os.chdir(EXAMPLE_DIR)
+    input_file = os.path.join(EXAMPLE_DIR, "config", "proffastpylot_parameters_tsukuba.yml")
     Pylot(input_file).run(n_processes=2)
