76fe382cc8c1e053edc8365ad9adda4ca8fce4b0
Moritz Makowski <moritz.makowski@tum.de>
2026-03-19 16:08:50 +0000

    #2 Run Pytest cases in CI (3)
    Debug example runs in CI

--- examples/run_tsukuba_retrieval.py
+++ examples/run_tsukuba_retrieval.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from prfpylot.pylot import Pylot
 from prfpylot.constants import EXAMPLE_DIR
