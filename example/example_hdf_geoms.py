71deb5a0640f2488e79f2c72bc05f62843fcd4f0
Moritz Makowski <moritz.makowski@tum.de>
2026-03-19 14:49:52 +0100

    #1 Add Pytest cases (3)
    update heidelberg mobile, tsukuba, and hdf geoms examples

--- example/example_hdf_geoms.py
+++ example/example_hdf_geoms.py
@@ -1,8 +1,13 @@
-from prfpylot.output.hdf_geoms_writer import GeomsGenWriter
+import os
+import sys
 
-geomsgen_inputfile = "input_sodankyla_hdf_geoms.yml"
-print(geomsgen_inputfile)
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from prfpylot.output.hdf_geoms_writer import GeomsGenWriter
+from prfpylot.constants import EXAMPLE_DIR
 
 if __name__ == "__main__":
+    os.chdir(EXAMPLE_DIR)
+    geomsgen_inputfile = os.path.join(EXAMPLE_DIR, "config", "input_sodankyla_hdf_geoms.yml")
     MyCreator = GeomsGenWriter(geomsgen_inputfile)
     MyCreator.generate_geoms_files()
--- example/run_heidelberg_mobile.py
+++ example/run_heidelberg_mobile.py
@@ -1,20 +1,31 @@
-from prfpylot.pylot import Pylot
+import os
+import sys
 
-proffastpylot_parameters = {
-    "instrument_number": "SN119",
-    "site_name": "Heidelberg",
-    "site_abbrev": "he",
-    "coord_file": "input_data/coords.csv",
-    "interferogram_path": "input_data/interferograms/SN119",
-    "map_path": "input_data/pressure_coords_mobile",
-    "pressure_path": "input_data/pressure_coords_mobile",
-    "pressure_type_file": "vaisala_pressure_parameters.yml",
-    "coord_type_file": "mira_coord_parameters.yml",
-    "analysis_path": "analysis",
-    "result_path": "results",
-    "coord_path": "input_data/pressure_coords_mobile",
-    "altitude_factor": 1e-3,  # conversion to km
-}
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from prfpylot.pylot import Pylot
+from prfpylot.constants import EXAMPLE_DIR
+
+CONFIG_DIR = os.path.join(EXAMPLE_DIR, "config")
+DATA_DIR = os.path.join(EXAMPLE_DIR, "data")
+INPUT_DATA_DIR = os.path.join(EXAMPLE_DIR, "data", "input_data")
+
 
 if __name__ == "__main__":
+    proffastpylot_parameters = {
+        "instrument_number": "SN119",
+        "site_name": "Heidelberg",
+        "site_abbrev": "he",
+        "coord_file": os.path.join(INPUT_DATA_DIR, "coords.csv"),
+        "interferogram_path": os.path.join(INPUT_DATA_DIR, "interferograms/SN119"),
+        "map_path": os.path.join(INPUT_DATA_DIR, "pressure_coords_mobile"),
+        "pressure_path": os.path.join(INPUT_DATA_DIR, "pressure_coords_mobile"),
+        "pressure_type_file": os.path.join(CONFIG_DIR, "vaisala_pressure_parameters.yml"),
+        "coord_type_file": os.path.join(CONFIG_DIR, "mira_coord_parameters.yml"),
+        "analysis_path": os.path.join(DATA_DIR, "analysis"),
+        "result_path": os.path.join(DATA_DIR, "results"),
+        "coord_path": os.path.join(INPUT_DATA_DIR, "pressure_coords_mobile"),
+        "altitude_factor": 1e-3,  # conversion to km
+    }
+
     Pylot(proffastpylot_parameters).run()
